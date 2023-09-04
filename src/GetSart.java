03207ddbf38c41567782c301a303bcf717f8ddd6
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2023-09-04 17:08:59 +0600

    Let's get start with java programming language.

---
+++ src/GetSart.java
@@ -0,0 +1,16 @@
+// This is a Java program. It starts with defining a class named "GetSart".
+public class GetSart {
+    // This is the main method. It's the entry point of the program.
+    // The 'public' keyword means that this method can be accessed from outside the class.
+    // 'static' means the method belongs to the class itself, not an instance of the class.
+    // 'void' indicates that the method does not return any value.
+    // 'main' is the method name.
+    // '(String[] args)' is the parameter list, allowing you to pass arguments to the program.
+    public static void main(String[] args) {
+        // This line uses the 'System' class to access the standard output stream.
+        // 'System.out' represents the standard output (usually the console).
+        // 'println' is a method of 'out' that prints a line of text to the console.
+        // "Hello Java..." is the text that will be printed to the console.
+        System.out.println("Hello Java...");
+    }
+}
