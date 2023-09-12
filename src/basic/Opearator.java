3cd5d23c9d4e8befc70ba84c485b528c74e0feb9
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2023-09-12 06:24:13 +0600

    Operators Added

---
+++ src/basic/Opearator.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class Opearator {
+    public static void main(String[] args) {
+        int sum = 10 + 20;
+        /*
+         * here 10 + 20 is expression
+         * and + is operator
+         * 10, 20 is operand
+         */
+
+        /*
+         * There are 7 types of opearator. There are :
+         * 1. Arithmetic Opearator ( +, -, *, /, %, etc)
+         * 2. Relational Operator ( ==, >, <, >=, <=, !=, etc)
+         * 3. Logical Operator ( &&, ||, ! )
+         * 4. Bitwise Operator ( &, |, ^, <<, >>, ~)
+         * 5. Assignment Operator ( =, +=, -=, *=, /=, %=, etc)
+         * 6. Conditional Operator ( ?, :)
+         * 7. Special Operator (".", ",") 
+         */
+    }
+}
